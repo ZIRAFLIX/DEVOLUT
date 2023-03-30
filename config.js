b983eda2246f5556d709577db2e059bd9d3f8209
Mateus Magalhães <mixzira@outlook.com.br>
2023-03-30 19:47:07 -0300

    Created random activity system.

---
+++ config.js
@@ -0,0 +1,13 @@
+export default {
+    activity: { // Activity application configuration
+        time: 1, // Time to change activity in minutes
+        type: 0, // Available types: GAME: 0, STREAMING: 1, LISTENING: 2, WATCHING: 3, COMPETING: 5
+        random: [ // Content to change activity
+            `Conteúdo 1`,
+            `Conteúdo 2`,
+            `Conteúdo 3`,
+            `Conteúdo 4`,
+            `Conteúdo 5`
+        ]
+    }
+}
--- src/events/system/ready.js
+++ src/events/system/ready.js
@@ -1,6 +1,7 @@
 import baseEvent from '../../utils/baseEvent.js';
 import mysql from '../../database/mysql.js';
 import discordRegister from '../../database/models/discordRegister.js';
+import config from '../../../config.js';
 
 export default class extends baseEvent {
     constructor(client) {
@@ -9,11 +10,16 @@ export default class extends baseEvent {
         })
     }
     run = async () => {
-        await mysql.authenticate().then(() => {
-            discordRegister.init(mysql);
-            discordRegister.sync();
-        }).catch((err) => console.log(err));
-        this.client.registryCommands();
-        console.log(this.client.user.tag + ' - Conexão com o Discord estabelecida com sucesso!');
+        await mysql.authenticate().then(() => { // Connect to database
+            discordRegister.init(mysql); // Initialize model of database
+            discordRegister.sync(); // Sync model with database
+        }).catch((err) => console.log(err)); 
+
+        this.client.registryCommands(); // Register commands of the application
+        console.log(this.client.user.tag + ' - Conexão com o Discord estabelecida com sucesso!'); // Log to console when the bot is ready
+
+        setInterval(() => { // Change activity every time defined in config.js
+            this.client.user.setActivity(`${config.activity.random[Math.floor(Math.random() * config.activity.random.length)]}`, {type: config.activity.type}); // Set activity with data from config.js
+        }, 1000 * 60 * config.activity.time);
     }
 }
